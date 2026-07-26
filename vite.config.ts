6774bf457262d4eaf645b5aaa88116f94b2e4f3a
goutamaec7 <goutam.aec7@gmail.com>
2026-07-26 08:57:06 +0530

    Update vite.config.ts
    for updating repo name as web site was blank after deployment

--- vite.config.ts
+++ vite.config.ts
@@ -16,3 +16,11 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  // Replace 'your-repo-name' with your exact GitHub repository name
+  base: '/ india-health-ed-mart /', 
+})
