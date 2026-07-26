c54415b575ffdffbf86502109eaf37391fa602e6
goutamaec7 <goutam.aec7@gmail.com>
2026-07-26 09:16:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Sets the base path for GitHub Pages deployment
-  base: "/india-health-ed-mart/", 
+  base: "/", 
   
   server: {
     host: "::",
